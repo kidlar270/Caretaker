5212aa0f7439375293ad6dba6d9a815598df290a
kidlar270 <kidlar270@gmail.com>
2026-02-13 18:45:14 +0300

    Refactor Firebase setup to include Firestore and Auth
    Removed analytics import and initialization, added Firestore and Auth services.

--- firebase.js
+++ firebase.js
@@ -1,12 +1,8 @@
-
-// Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
-import { getAnalytics } from "firebase/analytics";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
+import { getFirestore } from "firebase/firestore";
+import { getAuth } from "firebase/auth";
 
 // Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
   apiKey: "AIzaSyDX5KduBww9009TgJYS_sYEzzAIO2gefZk",
   authDomain: "kevo-4a76d.firebaseapp.com",
@@ -19,4 +15,8 @@ const firebaseConfig = {
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
-const analytics = getAnalytics(app);
+
+// Initialize Services and Export them for use in other files
+export const auth = getAuth(app);
+export const db = getFirestore(app);
+export default app;
